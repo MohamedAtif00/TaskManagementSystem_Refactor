924a387cfa56b7e720fd5d775e7557517c785948
EtshD1 <mohamed.h.abouelenin@gmail.com>
2023-05-21 19:17:06 +0300

    Remove old view

--- AutomatedTaskSystem.UI/src/pages/resources/users/index.tsx
+++ AutomatedTaskSystem.UI/src/pages/resources/users/index.tsx
@@ -1,14 +1,9 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../../app/hooks";
-import PlusIcon from "../../../assets/Icons/Plus";
-import QueryButton from "../../../components/button/queryButton";
 import AddUser from "../../../components/forms/addUser";
-import Header from "../../../components/header/header";
-import UserItem from "../../../components/userItem/userItem";
 import API from "../../../lib/API";
 import { clear, load } from "../../../slices/userSlice";
-import styles from "../../../styles/resources.module.scss";
 
 const Users = () => {
 	const users = useAppSelector((states) => states.usersSlice);
@@ -32,26 +27,7 @@ const Users = () => {
 	}, [dispatch]);
 
 	return (
-		<div className="container">
-			<Header text="Users" icon="Resources">
-				<QueryButton
-					icon={<PlusIcon />}
-					iconLeft
-					iconRight={false}
-					text="Add"
-					url={{
-						pathname: "/resources/users",
-						query: {
-							form: "user",
-						},
-					}}
-				/>
-			</Header>
-			<div className={styles.container}>
-				{users.map((g) => (
-					<UserItem key={g.id} group={g.group.name} name={g.name} />
-				))}
-			</div>
+		<div className="max-h-screen overflow-y-auto">
 			<AddUser />
 		</div>
 	);
