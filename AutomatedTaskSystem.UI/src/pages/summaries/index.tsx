9f18558abd6d5f63a0893d4dce0cbe9de316845f
EtshD1 <mohamed.h.abouelenin@gmail.com>
2023-06-26 12:28:13 +0300

    Change into table view

--- AutomatedTaskSystem.UI/src/pages/summaries/index.tsx
+++ AutomatedTaskSystem.UI/src/pages/summaries/index.tsx
@@ -6,55 +6,133 @@ import Header from "../../components/header/header";
 import API from "../../lib/API";
 import { load } from "../../slices/projectSlice";
 import Link from "next/link";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 interface Props {
-  id: number;
-  name: string;
-  description: string;
+	id: number;
+	name: string;
+	description: string;
 }
 
+const columns: GridColDef[] = [
+	{ field: "col0", headerName: "ID", width: 90 },
+	{
+		field: "col1",
+		headerName: "Name",
+		width: 300,
+	},
+	{ field: "col2", headerName: "Description", width: 300 },
+	{ field: "col3", headerName: "Year", width: 100 },
+	{ field: "col4", headerName: "Term", width: 100 },
+];
+
 const ProjectItem = ({ id, name, description }: Props) => {
-  return (
-    <Link href={`/reports/${id}`}>
-      <div className={styles2.GridItem} style={{ padding: "0.5rem 1rem" }}>
-        <div>
-          <div>{name}</div>
-          <div className={styles2.info}>{description}</div>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link href={`/reports/${id}`}>
+			<div className={styles2.GridItem} style={{ padding: "0.5rem 1rem" }}>
+				<div>
+					<div>{name}</div>
+					<div className={styles2.info}>{description}</div>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 const Projects = () => {
-  const projects = useAppSelector((states) => states.projectSlice);
-  const dispatch = useAppDispatch();
+	const projects = useAppSelector((states) => states.projectSlice);
+	const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    API.TASKS.PROJECTS().then((res) => {
-      if (res && !res.error) {
-        dispatch(load(res.data));
-      }
-    });
-  }, [dispatch]);
+	useEffect(() => {
+		API.TASKS.PROJECTS().then((res) => {
+			if (res && !res.error) {
+				dispatch(load(res.data));
+			}
+		});
+	}, [dispatch]);
 
-  return (
-    <div className="w-full">
-      <Header text="Projects" icon="Project" />
-      <div className={styles.container}>
-        {projects.map((p) => {
-          return (
-            <ProjectItem
-              id={p.id}
-              description={p.description}
-              name={p.name}
-              key={p.id}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="mx-auto relative max-h-screen overflow-y-auto pr-4">
+			<div className="bg-white border-solid border border-gray-300 rounded-b-md px-8 z-10 h-20 sticky top-0 left-0 right-0 flex items-center justify-between">
+				<Header text="Summaries" icon="Project" />
+			</div>
+			<div className="pb-4 mt-4">
+				<DataGrid
+					className="bg-white relative h-full"
+					slots={{
+						row: (r) => {
+							return (
+								<Link href={{ pathname: `/summaries/${r.rowId}` }}>
+									<div
+										key={r.rowId}
+										style={{ height: r.rowHeight }}
+										className="group hover:bg-slate-50 flex border-solid border-b border-slate-200"
+									>
+										{r.visibleColumns.map((c: any) => {
+											if (c.field === "col1")
+												return (
+													<div
+														key={c.headerName}
+														style={{
+															minWidth: c.width,
+															maxWidth: c.width,
+														}}
+														className="px-[0.625rem] group-hover:pl-4 transition-all ease-in text-base flex items-center group-hover:text-blue-700"
+													>
+														{r.row[c.field]}
+													</div>
+												);
+
+											return (
+												<div
+													key={c.headerName}
+													style={{
+														minWidth: c.width,
+														maxWidth: c.width,
+													}}
+													className="px-[0.625rem] group-hover:pl-4 transition-all ease-in text-sm flex items-center group-hover:text-blue-700"
+												>
+													{r.row[c.field]}
+												</div>
+											);
+										})}
+									</div>
+								</Link>
+							);
+						},
+					}}
+					rows={projects.map((p) => {
+						return {
+							id: p.id,
+							col0: p.id,
+							col1: p.name,
+							col2: p.description,
+							col3: p.year.name,
+							col4: p.term ? "Term 2" : "Term 1",
+						};
+					})}
+					columns={columns}
+				/>
+			</div>
+		</div>
+	);
+	return (
+		<div className="w-full">
+			<Header text="Projects" icon="Project" />
+			<div className={styles.container}>
+				{projects.map((p) => {
+					return (
+						<ProjectItem
+							id={p.id}
+							description={p.description}
+							name={p.name}
+							key={p.id}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
