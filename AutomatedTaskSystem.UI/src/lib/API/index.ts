e162dcf87f1c3d50f476ca365a1408a1ca7b0ad7
EtshD1 <mohamed.h.abouelenin@gmail.com>
2023-09-11 13:23:55 +0300

    Maintain Connection Strings

--- AutomatedTaskSystem.UI/src/lib/API/index.ts
+++ AutomatedTaskSystem.UI/src/lib/API/index.ts
@@ -4,7 +4,7 @@ import SCHEMAS from "./Schemas";
 import TASKS from "./tasks";
 import DASHBOARDS from "./dashboard";
 
-export const url = "http://localhost:5238";
+export const url = "/api";
 
 export interface BasicInfo {
     id: number;
